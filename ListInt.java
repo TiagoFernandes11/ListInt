2b8a24ede62f0f458dda072470ed05a0463b3d15
Tiago Fernandes Ribeiro <tiagofernandesribeiro@yahoo.com.br>
2023-04-21 14:36:58 -0300

    size()

--- ListInt.java
+++ ListInt.java
@@ -4,23 +4,31 @@ public class ListInt {
     public ListInt() {
     }
 
-    public void add(int n) {
-        if (node.getValor() == null) {
-            node = new Node(n);
+    public void add(int numero) {
+        if (node == null) {
+            node = new Node(numero);
             return;
         } else {
             Node aux = this.node;
             while (node.getNext() != null) {
                 aux = aux.getNext();
             }
-            aux.setNext(new Node(n));
+            aux.setNext(new Node(numero));
         }
     }
 
-    public int size(){
-        if(node.getValor() == null){
+    public int size() {
+        if (node == null) {
             return 0;
         }
+
+        int count = 1;
+        Node aux = this.node;
+        while (aux.getNext() != null) {
+            aux = aux.getNext();
+            count++;
+        }
+        return count;
     }
 
 }
